43d99fc330d017a44519efcc3aac90cc485a8ee2
JUSTIN NASSIE <justinnassie@Mac.attlocal.net>
2025-06-06 16:14:23 -0700

    Remove Clarion Partners from featured syndicators section - Keep them in main directory but not as featured partner

--- src/pages/Directory.tsx
+++ src/pages/Directory.tsx
@@ -335,7 +335,7 @@ export function Directory() {
           </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mb-8">
             {syndicators
-              .filter(s => s.company_name === 'Sutera Properties' || s.company_name === 'Back Bay Capital' || s.company_name === 'Starboard Realty' || s.company_name === 'Clarion Partners')
+              .filter(s => s.company_name === 'Sutera Properties' || s.company_name === 'Back Bay Capital' || s.company_name === 'Starboard Realty')
               .map((syndicator) => (
                 <Link
                   key={syndicator.id}
