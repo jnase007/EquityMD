25e668fa18181e03853bdc4cd69beb0a502ca1b6
JUSTIN NASSIE <justinnassie@JUSTINs-MacBook-Air-2.local>
2025-07-20 16:21:49 -0700

    Update Featured Syndicators count from 3+ to 20+ on homepage stats section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -611,7 +611,7 @@ export function Home() {
           <div className="grid md:grid-cols-3 gap-8 text-center">
             <StatCard number="$43M" label="Investor Requested Amount" icon={<Building className="h-8 w-8 text-blue-600" />} />
             <StatCard number="10K+" label="Accredited Investors" icon={<Briefcase className="h-8 w-8 text-blue-600" />} />
-            <StatCard number="3+" label="Featured Syndicators" icon={<Users className="h-8 w-8 text-blue-600" />} />
+            <StatCard number="20+" label="Featured Syndicators" icon={<Users className="h-8 w-8 text-blue-600" />} />
           </div>
         </div>
       </section>
